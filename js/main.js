13f67d491465f2bff05c120efcafb11af6c408ff
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2024-12-09 17:01:52 -0600

    updated

--- js/main.js
+++ js/main.js
@@ -18,3 +18,25 @@ $(function () {
     return false;
   });
 });
+
+// Number Counter Animation
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 0, 50, 3000);
+  counter("count2", 0, 5, 2500);
+  counter("count3", 0, 15, 3000);
+  counter("count4", 0, 11, 3000);
+});
